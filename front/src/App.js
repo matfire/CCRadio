86a48e71e1aed33c8e1c9dc224a5a33a3701e340
Florian Juniat <florian.juniat@epitech.eu>
2020-01-02 16:48:29 +0100

    add front web radio to see all the playlist

--- front/src/App.js
+++ front/src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div>
-        <Register/>
+        <Home/>
     </div>
   );
 }
